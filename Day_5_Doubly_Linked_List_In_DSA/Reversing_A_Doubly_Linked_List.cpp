7aabf80429caee2a528a743240def3992f861040
bibek376 <cu.18bcs6728@gmail.com>
2021-07-01 00:29:05 +0545

    After Updating

--- Day_5_Doubly_Linked_List_In_DSA/Reversing_A_Doubly_Linked_List.cpp
+++ Day_5_Doubly_Linked_List_In_DSA/Reversing_A_Doubly_Linked_List.cpp
@@ -31,6 +31,21 @@ node *Add_From_End(node *head,int data){
     return head;
 }
 
+node *Reverse_A_Doubly_Linked_List(node *head){
+    node *ptr1=head;
+    node *ptr2=ptr1->next;
+    ptr1->next=NULL;
+    ptr1->prev=ptr2;
+    while(ptr2 !=NULL){
+        ptr2->prev=ptr2->next;
+        ptr2->next=ptr1;
+        ptr1=ptr2;
+        ptr2=ptr2->prev;
+    }
+    head=ptr1;
+    return head;
+}
+
 void Display_Doubly_Linekd_List(node *head){
     if(head==NULL){
         cout<<"Linked List is empty"<<endl;
@@ -50,6 +65,10 @@ int main()
     head=Add_Empty(head,34);
     Add_From_End(head,45);
     Add_From_End(head,9);
+    Add_From_End(head,37);
+    Display_Doubly_Linekd_List(head);
+    cout<<"After Reversing a Linked List"<<endl;
+    head=Reverse_A_Doubly_Linked_List(head);
     Display_Doubly_Linekd_List(head);
     return 0;
 }
--- Day_5_Doubly_Linked_List_In_DSA/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-cout<<"After inserting a data in given index"<<endl;
-    // Inserting_At_a_Given_Index_In_Between(head,9,3);
-    // Display_Doubly_Linekd_List(head);
